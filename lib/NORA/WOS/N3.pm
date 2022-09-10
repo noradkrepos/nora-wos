7e398c3ee21536944756913f8c9fbbf9c5c6d8eb
franck <fkybus@gmail.com>
2022-09-10 15:56:31 +0200

    Added a type to all the adds so that we can split the RDF between multiple files. Added an output_nt.

--- lib/NORA/WOS/N3.pm
+++ lib/NORA/WOS/N3.pm
@@ -42,6 +42,9 @@ sub new
         'vivo'         => 'http://vivoweb.org/ontology/core#',
         'wos'          => 'https://wos.nora.dtu.dk/individual/',
         'xsd'          => 'http://www.w3.org/2001/XMLSchema#',
+        'opera'        => 'http://vivo.deffopera.dk/ontology/osrap/',
+        'orcid'        => 'https://orcid.org/',
+        'cla'          => 'https://www.nora.dtu.dk/ontology/clarivate/',
     };
     $self->{'pused'} = {};
     $self->{'data'} = {};
@@ -60,25 +63,26 @@ sub prefix
 
 sub add
 {
-    my ($self, $id, $code, @val) = @_;
+    my ($self, $type, $id, $code, @val) = @_;
 
     $id = $self->pused ('id', $id);
     $code = $self->pused ('code', $code);
-    if (!exists ($self->{'data'}{$id})) {
-        $self->{'data'}{$id} = {};
+    if (!exists ($self->{'data'}{$type}{$id})) {
+        $self->{'data'}{$type}{$id} = {};
     }
-    if (!exists ($self->{'data'}{$id}{$code})) {
-        $self->{'data'}{$id}{$code} = [];
+    if (!exists ($self->{'data'}{$type}{$id}{$code})) {
+        $self->{'data'}{$type}{$id}{$code} = {};
     }
     foreach my $val (@val) {
         $val = $self->pused ('value', $val);
-        push (@{$self->{'data'}{$id}{$code}}, $val);
+        $self->{'data'}{$type}{$id}{$code}{$val} = 1;
+
     }
 }
 
 sub add_text
 {
-    my ($self, $id, $code, @val) = @_;
+    my ($self, $type, $id, $code, @val) = @_;
 
     my @VAL = ();
     foreach my $val (@val) {
@@ -91,15 +95,20 @@ sub add_text
     }
     $id = $self->pused ('id', $id);
     $code = $self->pused ('code', $code);
-    if (!exists ($self->{'data'}{$id})) {
-        $self->{'data'}{$id} = {};
+    if (!exists ($self->{'data'}{$type}{$id})) {
+        $self->{'data'}{$type}{$id} = {};
     }
-    if (!exists ($self->{'data'}{$id}{$code})) {
-        $self->{'data'}{$id}{$code} = [];
+    if (!exists ($self->{'data'}{$type}{$id}{$code})) {
+        $self->{'data'}{$type}{$id}{$code} = {};
     }
     foreach my $val (@VAL) {
-        $val =~ s/"/\\"/g;
-        push (@{$self->{'data'}{$id}{$code}}, '"' . $val . '"');
+        if ($val =~ m/^[0-9]+$/) {
+            $self->{'data'}{$type}{$id}{$code}{$val} = 1;
+        } else {
+            $val =~ s/\\/\\\\/g;
+            $val =~ s/"/\\"/g;
+            $self->{'data'}{$type}{$id}{$code}{'"' . $val . '"'} = 1;
+        }
     }
 }
 
@@ -118,7 +127,7 @@ sub pused
         $self->{'pused'}{$pf} = 1;
         return ($code);
     } else {
-        if (($type ne 'code') || ($code ne 'a')) {
+        if ((($type ne 'code') || ($code ne 'a')) && ($code !~ m/^(true|false)$/)) {
             die ("fatal: missing prefix in $type: $code");
         }
         return ($code);
@@ -127,38 +136,127 @@ sub pused
 
 sub output
 {
-    my ($self, $fh) = @_;
+    my ($self, $basefile) = @_;
 
-    if (!defined ($fh)) {
-        die ("fatal: output requires a filehandle\n");
-    }
     my $len = 0;
     foreach my $key (sort (keys (%{$self->{'pused'}}))) {
         if (length ($key) > $len) {
             $len = length ($key);
         }
     }
+    $len++;
+    my $prefix = '';
     foreach my $key (sort (keys (%{$self->{'pused'}}))) {
-        printf ($fh "\@prefix %-${len}s <%s> .\n", $key, $self->{'prefix'}{$key});
+        $prefix .= sprintf ("\@prefix %-${len}s <%s> .\n", $key . ':', $self->{'prefix'}{$key});
     }
-    printf ($fh "\n");
-    foreach my $id (sort (keys (%{$self->{'data'}}))) {
-        printf ($fh "%s\n", $id);
-        my @codes = sort (keys (%{$self->{'data'}{$id}}));
-        my $code;
-        while ($code = shift (@codes)) {
-            my $pos = '.';
-            if (@codes) {
-                $pos = ';';
+    $prefix .= "\n";
+    my $fou;
+    if ($basefile eq '-') {
+        $fou = *STDOUT;
+        binmode ($fou, ':utf8');
+        print ($fou $prefix);
+    }
+    foreach my $type (sort (keys (%{$self->{'data'}}))) {
+        my $fc = '01';
+        my $lc = 60;
+        if ($basefile ne '-') {
+            if (!open ($fou, "> $basefile-$type-$fc.ttl")) {
+                die ("fatal: failed to open '$basefile-$type-$fc.ttl' for output: $!");
             }
-            if (length ($code) > 30) {
-                printf ($fh "    %s\n", $code);
-                printf ($fh "    %-30s %s %s\n", '', join (',', @{$self->{'data'}{$id}{$code}}), $pos);
-            } else {
-                printf ($fh "    %-30s %s %s\n", $code, join (',', @{$self->{'data'}{$id}{$code}}), $pos);
+            binmode ($fou, ':utf8');
+            print ($fou $prefix);
+        }
+        foreach my $id (sort (keys (%{$self->{'data'}{$type}}))) {
+            printf ($fou "%s\n", $id);
+            $lc++;
+            my @codes = sort (keys (%{$self->{'data'}{$type}{$id}}));
+            my $code;
+            while ($code = shift (@codes)) {
+                my $pos = '.';
+                if (@codes) {
+                    $pos = ';';
+                }
+                if (length ($code) > 30) {
+                    printf ($fou "    %s\n", $code);
+                    printf ($fou "    %-30s %s %s\n", '', join (' , ', sort (keys (%{$self->{'data'}{$type}{$id}{$code}}))), $pos);
+                    $lc += 2;
+                } else {
+                    printf ($fou "    %-30s %s %s\n", $code, join (' , ', sort (keys (%{$self->{'data'}{$type}{$id}{$code}}))), $pos);
+                    $lc++;
+                }
+            }
+            if ($lc >= 10000000) {
+                $fc = sprintf ('%02d', ($fc + 1));
+                close ($fou);
+                if (!open ($fou, "> $basefile-$type-$fc.ttl")) {
+                     die ("fatal: failed to open '$basefile-$type-$fc.ttl' for output: $!");
+                }
+                binmode ($fou, ':utf8');
+                print ($fou $prefix);
+                $lc = 60;
             }
         }
+        if ($basefile ne '-') {
+            close ($fou);
+        }
     }
+}
+
+sub output_nt
+{
+    my ($self, $basefile) = @_;
+
+    my $fou;
+    if ($basefile eq '-') {
+        $fou = *STDOUT;
+        binmode ($fou, ':utf8');
+    }
+    foreach my $type (sort (keys (%{$self->{'data'}}))) {
+        if ($basefile ne '-') {
+            if (!open ($fou, "> $basefile-$type.nt")) {
+                die ("fatal: failed to open '$basefile-$type.nt' for output: $!");
+            }
+            binmode ($fou, ':utf8');
+        }
+        foreach my $id (sort (keys (%{$self->{'data'}{$type}}))) {
+            my $nt1 = $self->output_nt_value ($id);
+            foreach my $code (sort (keys (%{$self->{'data'}{$type}{$id}}))) {
+                my $nt2 = $self->output_nt_value ($code);
+                foreach my $val (sort (keys (%{$self->{'data'}{$type}{$id}{$code}}))) {
+                    print ($fou join (' ', $nt1, $nt2, $self->output_nt_value ($val)), " .\n");
+                }
+            }
+        }
+        if ($basefile ne '-') {
+            close ($fou);
+        }
+    }
+}
+
+sub output_nt_value
+{
+    my ($self, $val) = @_;
+
+    if ($val =~ s/^([a-z]+)://) {
+        if ($self->{'prefix'}{$1}) {
+            return ('<' . $self->{'prefix'}{$1} . $val . '>');
+        } else {
+            die ("fatal: unknown prefix '$1' in: $1:$val\n");
+        }
+    }
+    if ($val =~ m/^[<"]/) {
+        return ($val);
+    }
+    if ($val eq 'a') {
+        return ('<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>');
+    }
+    if ($val =~ m/^[0-9]+$/) {
+        return ('"' . $val . '"^^<http://www.w3.org/2001/XMLSchema#integer>');
+    }
+    if ($val =~ m/^(true|false)$/) {
+        return ('"' . $val . '"^^<http://www.w3.org/2001/XMLSchema#boolean>');
+    }
+    die ("fatal: unexpected value: $val\n");
 }
 
 sub prefix_code
